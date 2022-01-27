388b38c188aa646639766ac069345fc111c3149a
Mattia <mattia.asti@gmail.com>
2022-01-27 15:54:49 +0000

    Fix env var

--- pages/api/1.js
+++ pages/api/1.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (process.env.VERCEL === "1") {
-    res.redirect(`https://${VERCEL_URL}/api/2`, 301);
+    res.redirect(`https://${process.env.VERCEL_URL}/api/2`, 301);
   } else {
     res.redirect("http://localhost:3000/api/2", 301);
   }
